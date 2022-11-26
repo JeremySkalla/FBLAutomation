3f32887d92e9037bd721d5226fad149ba60df55a
Jeremy Skalla <jeremy.skalla1@gmail.com>
2022-11-26 14:16:17 -0600

    Added comments and semicolons

--- server/index.js
+++ server/index.js
@@ -21,10 +21,11 @@ class Player{
     }
 }
 
+// Gets entire list of all players in range
 function getPlayers(data, x, y) {
-    var playerList = []
+    var playerList = [];
     for (var i=x; i<y; i++) {
-        let keys = Object.keys(data[i])
+        let keys = Object.keys(data[i]);
         for (const k of keys) {
             if (data[i][k] != " ") {
                 playerList.push(data[i][k])
@@ -35,24 +36,27 @@ function getPlayers(data, x, y) {
     return playerList
 }
 
-function pushToData(data, players, teams, x) {
+// Pushes rosters to end data
+function pushToData(data, players, teams, x) { 
     for (var i=0; i<players.length; i+=3) {
         let temp = new Player(players[i], players[i+1], players[i+2]);
         let j = (i/3) % x;
         data[teams[j]].push(temp.jsonify());
     }
 
-    return data;
+    return data
 }
 
 function processFile(file_name) {
-    const wb = reader.readFile(__dirname + '/' + file_name);
-    const data = reader.utils.sheet_to_json(wb.Sheets[wb.SheetNames[1]]);
+    const wb = reader.readFile(__dirname + '/' + file_name); // Reads file
+    const data = reader.utils.sheet_to_json(wb.Sheets[wb.SheetNames[1]]); // Gets data from sheet
 
+    // Initialization of variables
     var teamList1 = [];
     var teamList2 = [];
     let endData = {};
 
+    // Getting players
     for (var v in data[2]) {
         let temp = data[2][v].trim();
         if (temp != "VS" && temp != "") {
@@ -61,14 +65,15 @@ function processFile(file_name) {
         }
     }
 
+    // Pushing remaining two team names to proper places
     teamList2.push(data[17]['WEEK 1'].trim());
     endData[data[17]['WEEK 1'].trim()] = [];
     teamList2.push(data[17]['__EMPTY_5'].trim());
     endData[data[17]['__EMPTY_5'].trim()] = [];
 
+    // Function calls to do most of the work
     let playerList1 = getPlayers(data, 4, 12);
     let playerList2 = getPlayers(data, 19, 27);
-
     endData = pushToData(endData, playerList1, teamList1, 6);
     endData = pushToData(endData, playerList2, teamList2, 2);
 
@@ -76,7 +81,7 @@ function processFile(file_name) {
 }
 
 app.get("/processFile", (req, res) => {
-    const data = processFile(req.query.file);
+    const data = processFile(req.query.file); // Processing file to get rosters function
     console.log(data);
     res.json(data);
 });
